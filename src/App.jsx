56032d58e338d790daab73e902a7d3eb1748465a
KervisG <48578915+KervisG@users.noreply.github.com>
2024-04-12 06:13:24 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import LayoutHome from "./components/layout/LayoutHome";
+import '../src/styles/reset.scss'
+
+function App() {
+
+
+  return (  
+    <>
+    
+   <LayoutHome/>
+     
+    
+    </>
+  )
+}
+
+export default App
